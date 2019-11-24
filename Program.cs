77934dd095d536a04ea50644383387bc6ba411d2
kedar <kjoshi00@gmail.com>
2019-11-24 12:58:57 -0800

    Step 1: initial commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Calculator
 {
@@ -6,7 +7,37 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine();
+            var numbersToAdd = Sanitize(input);
+
+            Console.WriteLine(Add(numbersToAdd));
+            Console.Read();
+        }
+
+        private static int[] Sanitize(string input)
+        {
+            var numbersToAdd = input.Split(',')
+                                .Select(
+                                    number => {
+                                        try
+                                        {
+                                            return Convert.ToInt32(number);
+                                        } catch(Exception e)
+                                        {
+                                            return 0;
+                                        }
+                                    })
+                                .ToArray();
+            if (numbersToAdd.Length > 2)
+                throw new FormatException("Please provide only 2 numbers separated by a comma.");
+            
+
+            return numbersToAdd;
+        }
+
+        protected static int Add(int[] numbersToAdd)
+        {
+            return numbersToAdd.Sum();
         }
     }
 }
